2889cdcedb685d9af972358a968c678842f5b65e
Jason Thai <trit@yahoo-inc.com>
2016-05-03 15:21:31 -0700

    Foodtracker

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jason Thai on 5/3/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+}
